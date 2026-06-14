5369cd1f87b4c219daf7e48cbf58537ce51936d2
Misbah Uddin <misbah.06.uddin@gmail.com>
2026-06-15 03:28:11 +0530

    code changes

--- src/test/java/com/codedecode/restaurantlisting/RestaurantListingApplicationTests.java
+++ src/test/java/com/codedecode/restaurantlisting/RestaurantListingApplicationTests.java
@@ -1,13 +1,12 @@
 package com.codedecode.restaurantlisting;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RestaurantListingApplicationTests {
 
-	@Test
+	/*@Test
 	void contextLoads() {
-	}
+	}*/
 
 }
